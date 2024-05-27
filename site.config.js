7e20225c498ab947f00f3962c83dd2a6fea5e601
xxxdba <128507373+xxxdba@users.noreply.github.com>
2024-05-27 14:06:35 +0700

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -23,7 +23,7 @@ const CONFIG = {
   },
  
   // CONFIG configration (required)
-  link: "https://db10-blog.vercel.app",
+  link: "https://dba10-blog.vercel.app",
   since: 2022, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://github.com/xxxdba/blog-dba10/blob/main/public/apple-touch-icon.png", // The link to generate OG image, don't end with a slash
